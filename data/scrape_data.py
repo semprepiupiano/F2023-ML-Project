691b830f26436c36cda00b4c397acdb48933c746
Alex Suh <coolcatvas@gmail.com>
2023-12-02 23:43:23 -0500

    Add Ozone Data

--- data/scrape_data.py
+++ data/scrape_data.py
@@ -25,16 +25,25 @@ def get_site_codes(state_code, county_code):
     return [entry['code'] for entry in data['Data']]
 
 def get_all_parameter_codes():
-    data = requests.get(f"https://aqs.epa.gov/data/api/list/parametersByClass?email={EMAIL}&key={API_KEY}&pc=ALL").json()
-    return [entry['code'] for entry in data['Data']].join(",")
+    # data = requests.get(f"https://aqs.epa.gov/data/api/list/parametersByClass?email={EMAIL}&key={API_KEY}&pc=ALL").json()
+    # return ",".join([entry['code'] for entry in data['Data']])
+    # 42401: sulfur dioxide
+    # 87111: PM1 - Local Conditions
+    # 81101 - PM2.5 - Local Conditions
+    # 42101: Carbon monoxide
+    # 44201: Ozone
+    return "42401,87111,81101,42101,44201"
 
 def get_aqi_parameter_codes():
     data = requests.get(f"https://aqs.epa.gov/data/api/list/parametersByClass?email={EMAIL}&key={API_KEY}&pc=AQI POLLUTANTS").json()
     return ",".join([entry['code'] for entry in data['Data']])
 
 def get_site_data(state_code, county_code, site_code, bdate, edate, parameters):
-    URL = f"https://aqs.epa.gov/data/api/sampleData/bySite?email={EMAIL}&key={API_KEY}&param={parameters}&bdate={bdate}&edate={edate}&state={state_code}&county={county_code}&site={site_code}"
-    return requests.get(URL).json()['Data']
+    URL = f"https://aqs.epa.gov/data/api/dailyData/bySite?email={EMAIL}&key={API_KEY}&param={parameters}&bdate={bdate}&edate={edate}&state={state_code}&county={county_code}&site={site_code}"
+    print(URL)
+    result = requests.get(URL)
+    print(result)
+    return result.json()['Data']
 
 def get_aqi_data(state_code, county_code, site_code, bdate, edate):
     URL = f"https://aqs.epa.gov/data/api/dailyData/bySite?email={EMAIL}&key={API_KEY}&bdate={bdate}&edate={edate}&state={state_code}&county={county_code}&site={site_code}"
@@ -48,13 +57,20 @@ def main():
     state_code = get_state_code("Virginia")
     county_code = get_county_code(state_code, "Albemarle")
     site_code = get_site_codes(state_code, county_code)[0]
-    bdate = 20200101
-    edate = 20201231
-    parameters = get_aqi_parameter_codes()
-    aqi_data = get_aqi_data(state_code, county_code, site_code, bdate, edate)
-    with open("aqi_data.pkl", "wb") as f:
-        pickle.dump(aqi_data, f)
-    site_data = get_site_data(state_code, county_code, site_code, bdate, edate, parameters)
+    # bdate = 20200101
+    # edate = 20201231
+    byear = 2017
+    eyear = 2022
+    parameters = get_all_parameter_codes()
+    # print(parameters)
+    # aqi_data = get_aqi_data(state_code, county_code, site_code, bdate, edate)
+    # with open("aqi_data.pkl", "wb") as f:
+    #     pickle.dump(aqi_data, f)
+    site_data = dict()
+    for year in range(byear, eyear + 1):
+        bdate = f"{year}0101"
+        edate = f"{year}1231"
+        site_data[year] = get_site_data(state_code, county_code, site_code, bdate, edate, parameters)
     with open("site_data.pkl", "wb") as f:
         pickle.dump(site_data, f)
     # print(get_site_data(state_code, county_code, site_code, bdate, edate, parameters))
